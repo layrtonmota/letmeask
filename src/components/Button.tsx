c6be4045fef8f1958075087430e83a92cde3342a
Layrton <layrtonalberto@gmail.com>
2021-07-01 09:15:22 -0300

    segunda aula

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ButtonHTMLAttributes } from 'react'
+
+import '../styles/button.scss'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+
+export function Button (props: ButtonProps) {
+    
+
+    return (
+        <div>
+            <button className="button" {...props}></button>
+        </div>
+    )
+    
+}
